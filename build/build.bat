fc59d9864fd4523425fecadb229e0565afe02df2
thex <andreas.kar@gmx.at>
2018-05-27 23:18:38 +0200

    build script updated to latest scheme

--- build/build.bat
+++ build/build.bat
@@ -14,30 +14,29 @@ REM #                                                     #
 REM #######################################################
 
 REM --- Script Variables ---
-set remove_folders=1
-set log_files=0
-set title_hr=--------------------------------
+set scr_remove_folders=1
+set scr_log_files=0
 
-REM --- Packaging Variables ---
-set langs_upper=EN, IT, DE
-set default_lang=EN
-
+REM --- Project Variables ---
 set prj_id=gpb
 set prj_rev=v1.2.8
 set prj_name=gplus-badge
 set prj_fullname=Google Plus Badge - Particle
+set prj_title_hr=--------------------------------
+set prj_def_lang=EN
+set prj_sup_langs=EN, IT, DE
 
+REM --- Packaging Variables ---
 set pkg_part_only=particle.only
+set pkg_def_files=LICENSE, README.md
+set pkg_j3_def_files=LICENSE.pdf
+set pkg_expl_files=
+set pkg_lang_id=yaml
+set pkg_part_file_ext=yaml, html.twig
+
 set pkg_def_enable=1
 set pkg_leg_enable=1
 set pkg_helium_enable=1
 set pkg_hydro_enable=1
-
-REM --- File Variables ---
-set comp_files=
-set part_def_files=LICENSE, README.md
-set plugin_def_files=LICENSE.pdf
-set file_ext=yaml, html.twig
-set lang_ext=yaml
 
 "base-build.bat"
